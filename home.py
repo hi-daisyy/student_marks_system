ef5980e9a6f794ca583f68f77ec33a2bc7c6b09d
daisy <salenashrestha1818@gmail.com>
2026-03-12 20:55:30 +0545

    official added

--- home.py
+++ home.py
@@ -1,4 +1,4 @@
-print("Welcome to Student Marks System")
+print("Welcome to Official Student Marks System")
 print("Developed by daisy")
 
 # This will run exactly 3 times
@@ -6,4 +6,4 @@ for i in range(3):
     name = input("Enter student name: ")
     marks = float(input("Enter marks: "))
     print(name, "scored", marks)
-    print("---------") # Just to separate the entries
+    print("---------") # Just to separate the entrie
